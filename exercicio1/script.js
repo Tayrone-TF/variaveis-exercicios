8424e405682c0794ad080bfbe60562d58a726176
Tayrone Teixeira <108040192+Tayrone-TF@users.noreply.github.com>
2023-01-31 17:02:31 -0300

    Exercicios variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,57 @@
+//EXERCICIO 1
+
+// PARTE 1
+//a)
+// let nome
+
+//b)
+// let idade 
+
+//c)
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+//d) Nas duas variáveis foram impressos undefined, pois as variáveis não foram definidas na inicialização.
+
+//e)
+// nome = prompt('Qual é o seu nome?')
+// idade = Number(prompt('Qual é a sua idade?'))
+
+//f)
+// console.log(nome)
+// console.log(typeof nome)
+//a variável nome retornou um tipo "string" pois todo imput recebido através de um prompt retorna uma string;
+
+// console.log(idade)
+// console.log(typeof idade)
+// neste caso a variável idade retornou o tipo "number", pois antes do prompt guardar o valor na variável que seria uma "string", ela foi convertida para número através da função Number();
+
+//g)
+// console.log('Olá', nome, 'você tem', idade, 'anos')
+
+// PARTE 2 
+
+//a)
+// let pergunta1 = prompt('Você trabalha?')
+// let pergunta2 = prompt('Você estuda?')
+// let pergunta3 = prompt('Você tem carro?')
+
+//b)
+// console.log(trabalha)
+// console.log(estuda)
+// console.log(temCarro)
+
+//c)
+// let pergunta1 = 'você trabalha?'
+// let pergunta2 = 'Você estuda?'
+// let pergunta3 = 'Você tem carro?'
+
+//d)
+// let respostaPergunta1 = prompt(pergunta1)
+// let respostaPergunta2 = prompt(pergunta2)
+// let respostaPergunta3 = prompt(pergunta3)
+
+//e)
+// console.log(pergunta1, respostaPergunta1)
+// console.log(pergunta2, respostaPergunta2)
+// console.log(pergunta3, respostaPergunta3)
